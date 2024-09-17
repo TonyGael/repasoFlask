ee5cc4cdf17a80c077127fbeb296c85a8e624ca3
TonyGael <anthonygaelmayol@gmail.com>
2024-09-16 23:40:57 -0300

    agregando cambios

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask, request, make_response, redirect, render_template
 
 app = Flask(__name__)
 
+items = ['ITEM 1', 'ITEM 2', 'ITEM 3', 'ITEM 4']
+
 @app.route('/index')
 def index():
     user_ip_information = request.remote_addr
@@ -12,6 +14,10 @@ def index():
 @app.route('/show_information_address')
 def show_information_address():
     user_ip = request.cookies.get('user_ip_information')
-    return render_template('ip_information.html', user_ip = user_ip)
+    context = {
+        'user_ip' : user_ip,
+        'items' : items
+    }
+    return render_template('ip_information.html', **context)
 
 app.run(host='0.0.0.0', port=8080, debug=True)
